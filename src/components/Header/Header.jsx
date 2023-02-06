113f08846e2dac53d6052f44907a925a25a720c4
Ihor Kopieichykov <igor129k02@gmail.com>
2023-02-06 12:47:04 +0200

    header update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,23 @@ import './Header.scss';
 
 function Header() {
   return (
-    <header>
-        <h1>Cloth Shop</h1>
+    <header className='header'>
+        <div className="header__container">
+            <nav className='header__nav nav'>
+                <ul className='nav__list'>
+                    <li className='nav__item'>women</li>
+                    <li className='nav__item'>men</li>
+                    <li className='nav__item'>kids</li>
+                </ul>
+            </nav>
+            <div className='header__logo'>
+                <img src="../images/logo.svg" alt="logo" />
+            </div>
+            <div className='header__buttons '>
+                <button className='header__button-currency'>$</button>
+                <button className='header__button-cart'>cart</button>
+            </div>
+        </div>        
     </header>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import './styles/_nulling.scss'
+import './styles/_nulling.scss';
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
